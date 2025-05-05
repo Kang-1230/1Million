0f4b6e7044b35fa8f72db6f579ae25c76b67285b
Kang-1230 <tnwls7138@gmail.com>
2025-05-05 16:55:02 +0900

    WIP : 구글 로그인 연동 구현 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import WindowOpenButton from './components/WindowOpenButton';
 
 export default function Home() {
+  const client_id =
+    '607136172632-8f9b43ornn2ba464mt8ep1pts2eqemmm.apps.googleusercontent.com';
+  //.env.local 파일로 옮기기
+  const redirect_uri = 'http://localhost:3000/callback';
+  //배포 시 재등록 필요
+  const scope = [
+    'https://www.googleapis.com/auth/youtube.readonly',
+    'https://www.googleapis.com/auth/yt-analytics.readonly',
+    'https://www.googleapis.com/auth/yt-analytics-monetary.readonly',
+  ].join(' ');
+
+  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${encodeURIComponent(
+    scope
+  )}&response_type=token&access_type=online&prompt=consent`;
+
   return (
     <div className="w-full">
       <div className="min-w-[1344px] mx-[48px] h-[934px] my-[52px] bg-[#BA0000] flex flex-col items-center justify-center rounded-[8px]">
@@ -19,7 +34,9 @@ export default function Home() {
           의 본인 채널이 만들어져 있어야 로그인이 가능해요!
         </h4>
         <div>
-          <button>Google로 로그인</button>
+          <button onClick={() => (window.location.href = authUrl)}>
+            Google로 로그인
+          </button>
         </div>
       </div>
     </div>
